5a60d0f7dd3fd761e797968a523473abe738f96d
David Harris <davidharris319@gmail.com>
2021-03-29 18:13:08 -0400

    updated about component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => (
       <div className="text-box">
         <h3>Our Story</h3>
         <p>
-          Since 1985, Splash has stocked quality giftware for Canadian retailers. Now considered on of the top suppliers in Canada, we started our journey with five employees, a small office and an undersized warehouse
+          Since 1985, Splash has stocked quality giftware for Canadian retailers. Now considered on of the top suppliers in Canada, we started our journey with five employees, a small office and an undersized warehouse.
         </p>
         <a href="/about">Learn More</a>
       </div>
