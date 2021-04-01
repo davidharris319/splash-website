bf67f60cdd9828e89e665464a66c6da6016c3751
David Harris <davidharris319@gmail.com>
2021-03-31 22:34:45 -0400

    fixed padding or catalogue section, fixed orientation of catalogue and new customer sections

--- src/components/NewCustomer/NewCustomer.jsx
+++ src/components/NewCustomer/NewCustomer.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import "./NewCustomer.css";
 
-import worldMap from "../../images/product_pictures/world_map.jpg";
+import wBank from "../../images/product_pictures/wedding_bank.jpg";
 
 const NewCustomer = () => {
   return (
     <section id="newCustomer" className="new-customer-container">
-    <div className="imgBox">
-    <img src={worldMap} alt="World Map" />
-    </div>
+      <div className="imgBox mobile">
+        <img src={wBank} alt="Wedding Bank" />
+      </div>
       <div className="text-box">
         <h1>
-         Thank you for your interest in Splash
+        Thank you for your interest in Splash
         </h1>
         <p>
-        Serving the wholesale market only. Interested in become a retailer? Apply today to view our entire product line and gain access to our wholesale pricing!
-         </p>
+        Splash serves the wholesale market only. Interested in become a retailer? Apply today to view our entire product line and gain access to our wholesale pricing!
+        </p>
         <br />
         <a
           href="http://splash.remotecatalog.com/getNewClient.php"
@@ -24,6 +24,9 @@ const NewCustomer = () => {
         >
           <button>Apply Now </button>
         </a>
+      </div>
+      <div className="imgBox">
+        <img src={wBank} alt="World Map" />
       </div>
     </section>
   );
